c26a69ff9e781a1da3d41adc36ce95d3d760f50e
SAndrade22 <85464011+SAndrade22@users.noreply.github.com>
2023-05-03 08:47:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SAndrade-Angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-r3ptbs)
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './main.html',
+  styleUrls: ['./main.css'],
+})
+export class App {
+  name = 'Lisseth';
+  cargo = 'Doente';
+  a = 100;
+  b = 0;
+  c = 0;
+  contador = 0;
+
+  calcular() {
+    console.log('Hola UPS', this.a);
+    this.contador = this.contador + 1;
+    this.c = 0 + this.a + eval('' + this.b);
+  }
+}
+
+bootstrapApplication(App);
