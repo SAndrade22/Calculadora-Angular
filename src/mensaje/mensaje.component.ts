bb4ca0f27267465c1c2e701bc90e6138a636abf0
SAndrade22 <85464011+SAndrade22@users.noreply.github.com>
2023-05-04 08:02:59 -0500

    2 files modified

--- src/mensaje/mensaje.component.ts
+++ src/mensaje/mensaje.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mensaje',
   templateUrl: './mensaje.component.html',
+  standalone: true,
   styleUrls: ['./mensaje.component.css']
 })
 export class MensajeComponent implements OnInit {
